b53b3a15756e61c36c05cb6496872db2d766ee4f
Anibal Ventura <anibalventura20@gmail.com>
2020-12-30 19:24:55 -0400

    Initial Commit

---
+++ LuckyThree/LuckyThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyThreeApp.swift
+//  LuckyThree
+//
+//  Created by Anibal Ventura on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct LuckyThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
